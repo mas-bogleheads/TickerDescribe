2df342292ae35e9b9a043e9259631a820c928bf9
mas-bogleheads <mstewart-signup@zoho.com>
2015-03-11 23:42:59 -0700

    Changed updateURL and downloadURL (not pointing at userscripts.org). Allow this to work with https bogleheads URL. Increment version number.

---
+++ tickerdescribe.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name           TickerDescribe
+// @namespace      http://mas.xtreemhost.com/ticker_describe/
+// @version        1.3.20150311
+// @description    Provide description for mutual fund, and ETF tickers on Bogleheads forum (phpBB3) - Updated with data from 2012-11-10.
+// @include        http://www.bogleheads.org/forum/viewtopic.php*
+// @include        https://www.bogleheads.org/forum/viewtopic.php*
+// @updateURL      https://raw.githubusercontent.com/mas-bogleheads/TickerDescribe/master/tickerdescribe.meta.js
+// @downloadURL    https://raw.githubusercontent.com/mas-bogleheads/TickerDescribe/master/tickerdescribe.user.js
+// ==/UserScript==
--- tickerdescribe.user.js
+++ tickerdescribe.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name           TickerDescribe
 // @namespace      http://mas.xtreemhost.com/ticker_describe/
-// @version        1.2.20121110
+// @version        1.3.20150311
 // @description    Provide description for mutual fund, and ETF tickers on Bogleheads forum (phpBB3) - Updated with data from 2012-11-10.
 // @include        http://www.bogleheads.org/forum/viewtopic.php*
-// @updateURL      https://userscripts.org/scripts/source/87684.meta.js
-// @downloadURL    https://userscripts.org/scripts/source/87684.user.js
+// @include        https://www.bogleheads.org/forum/viewtopic.php*
+// @updateURL      https://raw.githubusercontent.com/mas-bogleheads/TickerDescribe/master/tickerdescribe.meta.js
+// @downloadURL    https://raw.githubusercontent.com/mas-bogleheads/TickerDescribe/master/tickerdescribe.user.js
 // ==/UserScript==
 
 
